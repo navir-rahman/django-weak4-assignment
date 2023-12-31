c696726083ab04cabb24394d7ed2d38aae3d08df
navir-rahman <navirrahman@gmail.com>
2024-01-01 00:37:13 +0600

    final push

--- assignment/views.py
+++ assignment/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from task.models import TaskModel
 from category.models import CategoryModel
-# Create your views here.
+
 def home(request):
-    data = TaskModel.objects.all()
-    categories = CategoryModel.objects.all()
-    return render(request, 'home.html', {'data': data, 'category': categories})
+    task = TaskModel.objects.all()
+
+    # categories = CategoryModel.objects.all()
+
+    return render(request, 'home.html', {'tasks': task})
--- task/models.py
+++ task/models.py
@@ -1,9 +1,11 @@
 from django.db import models 
-
+from category.models import CategoryModel
 class TaskModel(models.Model):
     task_title = models.CharField(max_length=20)
     task_description = models.CharField(max_length=100)
     is_completed = models.BooleanField(default=False)
+    category = models.ManyToManyField(CategoryModel)
+    
     assign_date = models.DateTimeField()
 
     def __str__(self):
